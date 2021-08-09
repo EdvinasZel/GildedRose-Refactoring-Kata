357a27a5323d1db88139dcbdb1265c2794689630
EdvinasZel <49122820+EdvinasZel@users.noreply.github.com>
2021-08-09 15:56:25 +0300

    Backstage pass tests
    Backstage pass tests added

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -60,4 +60,63 @@ class GildedRoseTest extends TestCase
         $this->assertEquals($item->quality, 50);
         $this->assertEquals($item->sell_in, -11);
     }
+    //Backstage pass tests
+    public function test_backstage_pass_before_sell_on_date_updates_quality()
+    {
+        $item = GildedRose::type('Backstage passes to a TAFKAL80ETC concert', 10, 10);
+        $item->updateQuality($item);
+
+        $this->assertEquals($item->quality, 12);
+        $this->assertEquals($item->sell_in, 9);
+    }
+    public function test_backstage_pass_updates_by_three_with_five_days_left_to_sell()
+    {
+        $item = GildedRose::type('Backstage passes to a TAFKAL80ETC concert', 10, 5);
+        $item->updateQuality($item);
+
+        $this->assertEquals($item->quality, 13);
+        $this->assertEquals($item->sell_in, 4);
+    }
+    public function test_backstage_pass_more_than_ten_days_before_sell_on_date_updates()
+    {
+        $item = GildedRose::type('Backstage passes to a TAFKAL80ETC concert', 10, 11);
+        $item->updateQuality($item);
+
+        $this->assertEquals($item->quality, 11);
+        $this->assertEquals($item->sell_in, 10);
+    }
+    public function test_backstage_pass_drops_to_zero_after_sell_in_date()
+    {
+        $item = GildedRose::type('Backstage passes to a TAFKAL80ETC concert', 10, 0);
+        $item->updateQuality($item);
+
+        $this->assertEquals($item->quality, 0);
+        $this->assertEquals($item->sell_in, -1);
+    }
+    public function test_backstage_pass_close_to_sell_in_date_with_max_quality()
+    {
+        $item = GildedRose::type('Backstage passes to a TAFKAL80ETC concert', 50, 10);
+        $item->updateQuality($item);
+
+        $this->assertEquals($item->quality, 50);
+        $this->assertEquals($item->sell_in, 9);
+    }
+    public function test_backstage_pass_very_to_sell_in_date_with_max_quality()
+    {
+        $item = GildedRose::type('Backstage passes to a TAFKAL80ETC concert', 50, 5);
+        $item->updateQuality($item);
+
+        $this->assertEquals($item->quality, 50);
+        $this->assertEquals($item->sell_in, 4);
+    }
+    public function test_backstage_pass_quality_zero_after_sell_date()
+    {
+        $item = GildedRose::type('Backstage passes to a TAFKAL80ETC concert', 50, -5);
+        $item->updateQuality($item);
+
+        $this->assertEquals($item->quality, 0);
+        $this->assertEquals($item->sell_in, -6);
+    }
+    
+
 }
